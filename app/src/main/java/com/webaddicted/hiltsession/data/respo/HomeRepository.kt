d22ffb42d9836e924bf1c3bfc90046862fb9b6b8
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-13 16:43:05 +0530

    Write Test cases

--- app/src/main/java/com/webaddicted/hiltsession/data/respo/HomeRepository.kt
+++ app/src/main/java/com/webaddicted/hiltsession/data/respo/HomeRepository.kt
@@ -1,19 +1,29 @@
 package com.webaddicted.hiltsession.data.respo
 
 import androidx.lifecycle.MutableLiveData
-import com.webaddicted.hiltsession.data.model.common.CommonRespo
-import com.webaddicted.hiltsession.data.model.home.AppVersionRespo
 import com.webaddicted.hiltsession.data.model.home.UserInfoRespo
 import com.webaddicted.hiltsession.data.model.img.SearchReq
 import com.webaddicted.hiltsession.data.model.img.SearchRespo
 import com.webaddicted.hiltsession.utils.apiutils.ApiResponse
 import com.webaddicted.hiltsession.utils.apiutils.DataFetchCall
 import com.webaddicted.hiltsession.utils.constant.ApiConstant
-import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.*
 import retrofit2.Response
 import javax.inject.Inject
 
 class HomeRepository @Inject constructor() : BaseRepository() {
+    fun getDbUserInfoApi(emailId:String, dbUserInfoRespo: MutableLiveData<UserInfoRespo>) {
+        GlobalScope.launch {
+          withContext{
+              var ssfds = userInfoDao.getUserInfoList(emailId) }
+            dbUserInfoRespo.postValue(respo.await())
+        }
+
+    }
+    fun setDbUserInfoApi(dbUserInfoRespo: MutableLiveData<UserInfoRespo>) {
+
+    }
+
     fun getImages(
         strUrl: SearchReq,
         userInfoRepo: MutableLiveData<ApiResponse<SearchRespo>>
@@ -41,43 +51,4 @@ class HomeRepository @Inject constructor() : BaseRepository() {
     }
 
 
-    fun userInfoApi(userInfoRepo: MutableLiveData<ApiResponse<CommonRespo<UserInfoRespo>>>) {
-        object : DataFetchCall<CommonRespo<UserInfoRespo>>(userInfoRepo) {
-            override fun createCallAsync(): Deferred<Response<CommonRespo<UserInfoRespo>>> {
-                return apiServices.getUserInfo()
-            }
-
-            override fun shouldFetchFromDB(): Boolean {
-                return false
-            }
-
-            override fun internetConnection(): Boolean {
-                return networkHelper.isNetworkConnected()
-            }
-        }.execute()
-    }
-
-    fun getAppVersion(userInfoRepo: MutableLiveData<ApiResponse<CommonRespo<AppVersionRespo>>>) {
-        object : DataFetchCall<CommonRespo<AppVersionRespo>>(userInfoRepo) {
-            override fun createCallAsync(): Deferred<Response<CommonRespo<AppVersionRespo>>> {
-                return apiServices.getAppVersion()
-            }
-
-            override fun shouldFetchFromDB(): Boolean {
-                return false
-            }
-
-            override fun internetConnection(): Boolean {
-                return networkHelper.isNetworkConnected()
-            }
-        }.execute()
-    }
-
-    fun setUpdateNotifyDays(days: Long) {
-        spManager.setUpdateNotifyDays(days)
-    }
-
-    fun getUpdateNotifyDays(): Long? {
-        return spManager.getUpdateNotifyDays()
-    }
 }
