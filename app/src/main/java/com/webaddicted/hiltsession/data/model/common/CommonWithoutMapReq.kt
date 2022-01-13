2c064f473b71e3fd6a2a7c2d1e84d124c3630829
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-13 16:42:23 +0530

    Write Test cases

--- app/src/main/java/com/webaddicted/hiltsession/data/model/common/CommonWithoutMapReq.kt
+++
@@ -1,9 +0,0 @@
-package com.webaddicted.hiltsession.data.model.common
-
-import com.google.gson.annotations.SerializedName
-
-
-data class CommonWithoutMapReq(
-    @SerializedName("queryParam")
-    val queryParam: String?
-)
