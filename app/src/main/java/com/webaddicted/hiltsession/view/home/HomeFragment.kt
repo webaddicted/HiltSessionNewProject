c9254b19af25ff775209c4b504d1f5282deab3ea
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-15 19:33:27 +0530

    Implement DB

--- app/src/main/java/com/webaddicted/hiltsession/view/home/HomeFragment.kt
+++ app/src/main/java/com/webaddicted/hiltsession/view/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.webaddicted.hiltsession.view.home
 
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -67,11 +66,6 @@ class HomeFragment : BaseFragment(R.layout.frm_home) {
                 else {
                     photoList = result?.data?.photos?.photo as ArrayList<Photo>?
                     setAdapter()
-//                    homeViewModel.setPrefUserInfo(it.data?.respDetails)
-                    Log.d(
-                        TAG,
-                        "User Info : ${it.data?.photos?.perpage}"
-                    )
                 }
             }
         })
