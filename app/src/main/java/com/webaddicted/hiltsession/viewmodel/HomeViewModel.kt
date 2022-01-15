5527d5c99e0fed6c17b948fc34ca2642ccefbc1e
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-15 19:33:33 +0530

    Implement DB

--- app/src/main/java/com/webaddicted/hiltsession/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/webaddicted/hiltsession/viewmodel/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.webaddicted.hiltsession.viewmodel
 
 import androidx.lifecycle.MutableLiveData
-import com.webaddicted.hiltsession.data.model.home.UserInfoRespo
 import com.webaddicted.hiltsession.data.model.img.SearchReq
 import com.webaddicted.hiltsession.data.model.img.SearchRespo
 import com.webaddicted.hiltsession.data.respo.HomeRepository
@@ -12,33 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repo: HomeRepository) :
     BaseViewModel() {
-    var getDbUserInfoRespo = MutableLiveData<UserInfoRespo>()
-    var setDbUserInfoRespo = MutableLiveData<UserInfoRespo>()
     var getImageRespo = MutableLiveData<ApiResponse<SearchRespo>>()
-
-    fun setPrefUserInfo(respo: UserInfoRespo?) {
-        val userModel = UserInfoRespo(
-            name = respo?.name,
-            email = respo?.email,
-            mobilePhone = respo?.mobilePhone,
-            address = respo?.address
-        )
-        return repo.setPrefUserInfo(userModel = userModel)
-    }
-
-    fun getPrefUserInfo(): UserInfoRespo? {
-        return repo.getPrefUserInfo()
-    }
-
-    fun getDbUserInfoApi() {
-        getDbUserInfoRespo = MutableLiveData<UserInfoRespo>()
-        repo.getDbUserInfoApi(getDbUserInfoRespo)
-    }
-
-    fun setDbUserInfoApi() {
-        setDbUserInfoRespo = MutableLiveData<UserInfoRespo>()
-        repo.setDbUserInfoApi(setDbUserInfoRespo)
-    }
 
     fun getImages(req: SearchReq) {
         getImageRespo = MutableLiveData<ApiResponse<SearchRespo>>()
